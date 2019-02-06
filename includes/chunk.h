5541d68bddc3608ddd4315ad96c34c5245a8605d
Alec Wilson <alecw1992@gmail.com>
2019-02-06 23:48:57 +0000

    Compress line information using run length encoding

--- includes/chunk.h
+++ includes/chunk.h
@@ -13,6 +13,8 @@ typedef struct {
 	int count;
 	int capacity;
 	uint8_t* code;
+	int lineCount;
+	int lineCapacity;
 	int* lines;
 	ValueArray constants;
 } Chunk;
@@ -21,6 +23,7 @@ void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
+int getLine(Chunk* chunk, int offset);
 
 #endif
 
--- src/chunk.c
+++ src/chunk.c
@@ -8,13 +8,15 @@ void initChunk(Chunk* chunk) {
 	chunk->count = 0;
 	chunk->capacity = 0;
 	chunk->code = NULL;
+	chunk->lineCount = 0;
+	chunk->lineCapacity = 0;
 	chunk->lines = NULL;
 	initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
 	FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-	FREE_ARRAY(int, chunk->lines, chunk->capacity);
+	FREE_ARRAY(int, chunk->lines, chunk->lineCapacity);
 	freeValueArray(&chunk->constants);
 	initChunk(chunk);
 }
@@ -24,15 +26,37 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
 		int oldCapacity = chunk->capacity;
 		chunk->capacity = GROW_CAPACITY(oldCapacity);
 		chunk->code = GROW_ARRAY(chunk->code, uint8_t, oldCapacity, chunk->capacity);
-		chunk->lines = GROW_ARRAY(chunk->lines, int, oldCapacity, chunk->capacity);
+	}
+	chunk->code[chunk->count] = byte;
+	chunk->count++;
+
+	if (chunk->lineCount == 0 || (chunk->lines[chunk->lineCount - 1] != line && chunk->lineCapacity < chunk->lineCount + 2)) {
+		int oldLineCapacity = chunk->lineCapacity;
+		chunk->lineCapacity = GROW_CAPACITY(oldLineCapacity);
+		chunk->lines = GROW_ARRAY(chunk->lines, int, oldLineCapacity, chunk->lineCapacity);
 	}
 
-	chunk->code[chunk->count] = byte;
-	chunk->lines[chunk->count] = line;
-	chunk->count++;
+	if (chunk->lineCount == 0 || chunk->lines[chunk->lineCount - 1] != line) {
+		chunk->lines[chunk->lineCount] = 1;
+		chunk->lines[chunk->lineCount + 1] = line;
+		chunk->lineCount += 2;
+	} else {
+		chunk->lines[chunk->lineCount - 2]++;
+	}
 }
 
 int addConstant(Chunk* chunk, Value value) {
 	writeValueArray(&chunk->constants, value);
 	return chunk->constants.count - 1;
 }
+
+int getLine(Chunk* chunk, int offset) {
+	int passedOffsets = 0;
+	for (int i = 0; i < chunk->lineCount; i+=2) {
+		passedOffsets += chunk->lines[i];
+		if (passedOffsets > offset) {
+			return chunk->lines[i + 1];
+		}
+	}
+}
+

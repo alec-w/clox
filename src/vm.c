5841cb125a50fd3668268aade037513141f15278
Alec Wilson <alecw1992@gmail.com>
2019-02-12 19:49:48 +0000

    Add stack to the vm

--- src/vm.c
+++ src/vm.c
@@ -5,10 +5,25 @@
 
 VM vm;
 
+static void resetStack() {
+	vm.stackTop = vm.stack;
+}
+
 void initVM() {
+	resetStack();
 }
 
 void freeVM() {
+}
+
+void push(Value value) {
+	*vm.stackTop = value;
+	vm.stackTop++;
+}
+
+Value pop() {
+	vm.stackTop--;
+	return *vm.stackTop;
 }
 
 static InterpretResult run() {
@@ -23,14 +38,20 @@ static InterpretResult run() {
 )
 	for(;;) {
 	#ifdef DEBUG_TRACE_EXECUTION
+		printf("          ");
+		for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+			printf("[ ");
+			printValue(*slot);
+			printf(" ]");
+		}
+		printf("\n");
 		disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 	#endif
 		uint8_t instruction;
 		switch (instruction = READ_BYTE()) {
 			case OP_CONSTANT: {
 				Value constant = READ_CONSTANT();
-				printValue(constant);
-				printf("\n");
+				push(constant);
 				break;
 			}
 			case OP_CONSTANT_LONG: {
@@ -40,6 +61,8 @@ static InterpretResult run() {
 				break;
 			}
 			case OP_RETURN:
+				printValue(pop());
+				printf("\n");
 				return INTERPRET_OK;
 		}
 	}
